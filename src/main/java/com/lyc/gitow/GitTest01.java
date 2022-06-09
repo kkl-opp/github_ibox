58465f79109068fc80d1be07a9d2a53523afbf11
lyc <1185137116@qq.com>
2022-06-09 09:49:55 +0800

    first commit

---
+++ src/main/java/com/lyc/gitow/GitTest01.java
@@ -0,0 +1,29 @@
+package com.lyc.gitow;
+
+/**
+ * @author lyc
+ * @version V1.0.0
+ * @date 2022/6/9 9:41
+ */
+public class GitTest01 {
+    public static void main(String[] args) {
+        Animals animals = new Animals("Panda", 8);
+        animals.eat();
+        System.out.println("hello git ");
+    }
+}
+
+class Animals {
+    String name;
+    int age;
+
+    public Animals(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void eat() {
+        System.out.println(name + "吃竹子");
+    }
+}
+
