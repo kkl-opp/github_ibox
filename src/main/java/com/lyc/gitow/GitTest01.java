325ae2b7f1060437a47bcc38473ae606dce0498f
lyc <1185137116@qq.com>
2022-06-10 22:30:00 +0800

    hot-fix 2

--- src/main/java/com/lyc/gitow/GitTest01.java
+++ src/main/java/com/lyc/gitow/GitTest01.java
@@ -14,7 +14,7 @@ public class GitTest01 {
         System.out.println("hello git2");
         System.out.println("hello scala");
         System.out.println("aaaaaaa");
-
+        System.out.println("hello spark2");
 
     }
 }
